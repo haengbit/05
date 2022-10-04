8113601743f6f3b6fd34e25f20004debc4772868
haengbit <biit4894@sookmyung.ac.kr>
2022-10-04 17:05:15 +0900

    5주차_실습3

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
+	char c;
+	int cnt = 0; // count 
 	
-	printf("input an integer : ");
-	scanf("%d", &x);
+	printf("input a string: ");
+	scanf("%d", &c);
 	
-	if (x < 0) // 음수인 경우 
-		x = x * -1;
-	printf("The absolute value is %i.\n", x);
-		
+	while ((c = getchar()) != '\n') // 입력 문자가 개행문자가 나올 때까지 반 
+	{
+		if (c >= '0' && c <= '9' ) // 입력된 글자가 숫자인가?
+			cnt = cnt + 1; // 그렇다면 하나를 센다 -> 숫자를 셀 수 있는 변수 하나 더 필요 
+	}
+	
+	printf("the number of digits is %i\n", cnt);
 	return 0;
 }
