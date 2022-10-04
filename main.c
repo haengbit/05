6bbcc73ba9a0eb2fd45223010082dcbb44913abe
haengbit <biit4894@sookmyung.ac.kr>
2022-10-04 17:47:18 +0900

    5주차_실습6

--- main.c
+++ main.c
@@ -4,44 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-
-	int x, y;
-	char op;
-	int sum;
+	int answer = 59;
+	int x; // 받는 입력에 대한 저장 변수 
+	int trial = 0;
 	
-	// int 정수형이어서 소수점 잘 안 나와도 ok 
-	printf("enter the calculation : ");
-	scanf("%d %c %d", &x, &op, &y); 
+	do
+	{
+		printf("Guess a number : "); // 입력해라 문구 출력 	
+		scanf("%d", &x);             // 입력을 받음 (scanf)
+		if (x > answer)
+			printf("high!\n");
+		else if (x < answer)
+			printf("low!\n");         // 입력 숫자가 정답보다 큰지 작은지 출력 - high/low
+		trial++;
+	}	
+	while ( x != answer );           // 정답과 같은지 여부 판단 
 	
-	if (op == '+')
-		sum = x + y;
-	else if (op == '-')
-		sum = x - y;
-	else if (op == '*')
-		sum = x * y;
-	else
-		sum = x / y; 
-		
-//	switch(op)
-//	{
-//		case '+':
-//			sum = x+y;
-//			break;
-//			
-//		case '-':
-//			sum = x-y;
-//			break;
-//			
-//		case '*':
-//			sum = x*y;
-//			break;
-//			
-//		default:
-//			sum = x / y;
-//			break;
-//	}
-	
-	printf(" = %i\n", sum);
-	
+	// 시도 횟수 출력
+	printf("Congratulation! trials : %i\n", trial); 
 	return 0;
 }
