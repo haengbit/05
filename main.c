46a00a213e300adbab413028e0bd3d87c92df35c
haengbit <biit4894@sookmyung.ac.kr>
2022-10-04 17:19:34 +0900

    3주차_실습4

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int cnt = 0; // count 
+
+	int sum = 0;
+	int x; 
+	int i;
 	
-	printf("input a string: ");
-	scanf("%d", &c);
+	printf("input a number: ");
+	scanf("%d", &x);
 	
-	while ((c = getchar()) != '\n') // 입력 문자가 개행문자가 나올 때까지 반 
-	{
-		if (c >= '0' && c <= '9' ) // 입력된 글자가 숫자인가?
-			cnt = cnt + 1; // 그렇다면 하나를 센다 -> 숫자를 셀 수 있는 변수 하나 더 필요 
-	}
+	for (i = 0 ; i <= x; i++)
+		sum = sum + i;
 	
-	printf("the number of digits is %i\n", cnt);
+	printf("The result is %i\n", sum);
 	return 0;
 }
